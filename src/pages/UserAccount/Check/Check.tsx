7dd0dde1f1d0840e10cbe9cf773c66c9530f7fd5
Runar <runarnazmutdinov@gmail.com>
2022-11-28 23:53:59 +0300

    make a table of invoices for the provision of services

--- src/pages/UserAccount/Check/Check.tsx
+++ src/pages/UserAccount/Check/Check.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react'
 import { Button } from '../../../components/ui-kit/Button/Button'
+
+import cn from "classnames"
 import classes from "./Check.module.scss"
 
 export const Check: FC = () => {
@@ -11,9 +13,37 @@ export const Check: FC = () => {
             <h1 className={classes.title}>Добрый день, Dmitriy!</h1>
             <h1 className={classes.title}>Вам выставлен счет на оказание услуги</h1>
           </div>
-
           <div className={classes.table}>
-            <h1>Table</h1>
+            <div className={classes.fieldNames}>
+              <span className={classes.description}>Описание</span>
+              <span className={classes.service}>Услуга</span>
+              <span className={classes.rate}>Тариф</span>
+              <span className={classes.price}>Цена (BYN)</span>
+              <span className={classes.quantity}>Количество</span>
+              <span className={classes.total}>Итого</span>
+            </div>
+            <div className={classes.fieldValues}>
+              <span className={cn(classes.description, classes.style)}>
+                <span className={classes.id}>ID: 4813</span>
+                <span className={classes.address}> Адрес: Минск, бр. Мулявина, д.10</span>
+              </span>
+              <span className={cn(classes.service, classes.style)}>Размещение</span>
+              <span className={cn(classes.rate, classes.style)}>Топ</span>
+              <span className={cn(classes.price, classes.style)}>30.00</span>
+              <span className={cn(classes.quantity, classes.style)}>1</span>
+              <span className={cn(classes.total, classes.style)}>30.00</span>
+            </div>
+            <div className={classes.totalPayable}>
+
+              <div className={classes.discount}>
+                <span>Скидка:</span>
+                <span>5.00 BYN</span>
+              </div>
+              <div className={classes.totalPrice}>
+                <span>Итого к оплате:</span>
+                <span>25.00 BYN</span>
+              </div>
+            </div>
           </div>
           <div className={classes.buttons}>
             <Button className={classes.payLaterBtn}>Оплачу позже</Button>
